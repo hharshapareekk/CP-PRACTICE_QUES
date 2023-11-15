b3555de48b05c25cd4aff0374b3c223b31adc8c0
Harsha Pareek <harshapareek14@gmail.com>
2023-11-15 17:56:59 +0530

    logic done code left

---
+++ nov2023/14nov-1.cpp
@@ -0,0 +1,25 @@
+//13 nov '23 - codeforces :https://codeforces.com/problemset/problem/1884/A
+//logic done code left 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,k;
+        cin>>x>>k;
+        if(k>=10 )
+        {
+
+        }
+        else
+        {
+            
+        }
+    }
+    return 0;
+}
