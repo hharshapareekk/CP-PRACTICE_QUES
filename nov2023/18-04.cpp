6e30095487b9f5cb369b2b7283b24cc03065c37f
Harsha Pareek <harshapareek14@gmail.com>
2023-11-21 20:58:13 +0530

    up

--- nov2023/18-04.cpp
+++ nov2023/18-04.cpp
@@ -1,69 +1,16 @@
-// Codes Of Harsha Pareek
-// Problem Statement :  https://codeforces.com/problemset/problem/1878/C
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-int main()
-{
-    ll t;
-    cin >> t;
-    while (t--)
-    {
-        ll n, k, x;
-        cin >> n >> k >> x;
-        if (n > x && k != 0)
-        {
-            cout << "NO" << endl;
-        }
-        else if (n == x && k > 1)
-        {
-            cout << "NO" << endl;
-        }
-        else
-        {
-            float sol = n;
-            float low = 0;
-            float high = 0;
-            float div = (float)(x - n) / (float)(k - 1);
-            if (div == (ceil)(div))
-            {
-                sol += div;
-                low = div - 1;
-                high = div + 1;
-                k --;
-            }
-            else
-            {
-                low = (floor)(div);
-                high = (ceil)(div);
-            }
+ #include <iostream>
 
-            k --;
-            int temp = 1;
-            while (temp<=k && sol != x)
-            {
-                if (temp %  2 != 0)
-                {
-                    sol +=low;
-                    low--;
-                    temp++;
-                }
-                else
-                {
-                    sol+=high;
-                    high++;
-                    temp++;
-                }
-            }
-            if(sol == x)
-            {
-                cout<<"YES"<<endl;
-            }
-            else
-            {
-                cout<<"NO"<<endl;
-            }
-        }
+using namespace std;
+
+int main(){
+	int t; //read the number of test cases
+	cin >> t;
+	while(t--){
+		long long n, x, k; //read n, x, k for each test case
+		cin >> n >> x >> k;
+		if(2*k>=x*(x+1) && 2*k<=n*(n+1)-(n-x)*(n-x+1)){ //check if k is between the minimum and maximum sum 
+			cout << "YES\n";
+		}
+		else cout << "NO\n";
     }
-    return 0;
 }
