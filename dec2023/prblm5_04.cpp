90e28099c3ea5a0fd7a58ab87aaaa6d4b3efa6de
Harsha Pareek <harshapareek14@gmail.com>
2023-12-04 20:59:33 +0530

    updt

--- dec2023/prblm5_04.cpp
+++ dec2023/prblm5_04.cpp
@@ -1,18 +1,37 @@
-//Codes Of Harsha Pareek
-//Problem Statement :
+// Codes Of Harsha Pareek
+// Problem Statement :
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 int main()
 {
     ll t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         ll n;
-        cin>>n;
+        cin >> n;
         string str;
-        cin>>str;
+        cin >> str;
+        int flag =0;
+        for(int i= 0 ;i <= (n-1) ;i++)
+        {
+            if(str[i] == '0')
+            {
+                cout<<"YES"<<endl;
+                flag =1;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        if(flag == 0)
+        {
+            cout<<"NO"<<endl;
+        }
     }
+
     return 0;
 }
---
+++ dec2023/sample.cpp
@@ -0,0 +1,11 @@
+//Codes Of Harsha Pareek
+//Problem Statement :
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    string str = "abc";
+    cout<<str[2];
+    return 0;
+}
